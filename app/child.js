8d2481ebe0892cd8e502028c3a8377b7fff75252
Tom Scott <tomscott85@gmail.com>
2017-01-27 08:16:16 +1100

    latest changes

---
+++ app/child.js
@@ -0,0 +1,18 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+ReactDOM.render(<ShowList />, document.getElementById('app'));
--- app/index.js
+++ app/index.js
@@ -1,12 +1,24 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
-var HelloUser = React.createClass({
+var _ = require('lodash')
+var ShowList = require('./child.js')
+var FriendsContainer = React.createClass({
   render: function(){
+    var name = 'Tyler McGinnis'
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
     return (
       <div>
-        Hello {this.props.name}
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
       </div>
     )
   }
 });
-ReactDOM.render(<HelloUser name="Bill"/>, document.getElementById('app'));
+ReactDOM.render(<FriendsContainer />, document.getElementById('app'));
+
+
+// var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen'];
+// var listItems = friends.map(function(friend){
+//   return "<li> " + friend + "</li>";
+// });
+// console.log(listItems); // ["<li> Ean Platter </li>", "<li> Murphy Randall</li>", "<li> Merrick Christensen </li>"];
